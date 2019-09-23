194d45d8d7cd384e69863c20b214a0b5b68ed03f
s184213 <s184213@student.dtu.dk>
2019-09-23 02:49:55 +0200

    Might be able to calculate pulse with int

--- qsr.c
+++ qsr.c
@@ -8,7 +8,7 @@ void peakDetection(QRS_params *params)
 }
 
 int findPulse(int RP_size, int time) {
-    float seconds = time/250;
-    float beats_pr_min = (RP_size/seconds)*60;
-    return (int)  beats_pr_min;
+    int milliseconds = time*1000/250;
+    int beats_pr_min = (RP_size*1000)/milliseconds*60;
+    return beats_pr_min;
 }
